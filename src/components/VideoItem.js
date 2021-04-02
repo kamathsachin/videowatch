04f160dd7b91c3b303ef04751857260ff60c0ee9
Sachin Kamath <kamathsachin@hotmail.com>
2021-04-01 23:47:43 -0500

    changes made to display the thumbnail and title of the videos

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const VideoItem = (props) => {
-  return <div>Video Item</div>;
+const VideoItem = ({ video }) => {
+  console.log(video);
+  return (
+    <div>
+      <img src={video.snippet.thumbnails.medium.url} alt="" />
+      {video.snippet.title}
+    </div>
+  );
 };
 
 export default VideoItem;

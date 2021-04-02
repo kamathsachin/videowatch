4d5a43a14a15d3964879609b2551eb80ca03fecb
Sachin Kamath <kamathsachin@hotmail.com>
2021-04-01 23:12:10 -0500

    completed changes for Search Bar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../api/YouTube";
 
 class App extends Component {
+  state = { videos: [] };
+
+  onSearchTermSubmit = async (term) => {
+    const response = await youtube.get("/search", { params: { q: term } });
+
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onSearchTermSubmit} />I have{" "}
+        {this.state.videos.length} videos found
       </div>
     );
   }

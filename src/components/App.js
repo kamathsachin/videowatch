e80275de21ebbacfe05b828d5da30847d9cca507
Sachin Kamath <kamathsachin@hotmail.com>
2021-04-02 23:22:27 -0500

    added the final component to display the video

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,17 @@ import youtube from "../api/YouTube";
 class App extends Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onSearchTermSubmit("Buildings");
+  }
+
   onSearchTermSubmit = async (term) => {
     const response = await youtube.get("/search", { params: { q: term } });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = (video) => {
@@ -21,11 +28,19 @@ class App extends Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onSearchTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,12 +2,19 @@ import React from "react";
 
 const VideoDetail = ({ video }) => {
   if (!video) {
-    return <div>Select Video</div>;
+    return <div>Loading...</div>;
   }
+
+  const VideoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
   return (
-    <div className="ui segment">
-      <h4 className="ui header">{video.snippet.title}</h4>
-      <p>{video.snippet.description}</p>
+    <div>
+      <div className="ui embed">
+        <iframe title="selected video" src={VideoSrc} />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
     </div>
   );
 };

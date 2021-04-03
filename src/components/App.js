bc0d323f506310b5afdea9a81eadf8c64eeeead6
Sachin Kamath <kamathsachin@hotmail.com>
2021-04-02 22:05:35 -0500

    made changes to send the selected video to the parent i.e. App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import VideoList from "./VideoList";
 import youtube from "../api/YouTube";
 
 class App extends Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onSearchTermSubmit = async (term) => {
     const response = await youtube.get("/search", { params: { q: term } });
@@ -12,11 +12,18 @@ class App extends Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = (video) => {
+    console.log(video);
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onSearchTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
